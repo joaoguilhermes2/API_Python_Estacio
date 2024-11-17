ff12397ff00706a144db69ac7f59eb1d63712f9b
Elias <eliasmqs397@gmail.com>
2024-11-17 08:52:21 -0400

    fix: bugs

--- register.py
+++ register.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import boto3
 import uuid
 import os
@@ -6,10 +7,11 @@ import json
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+CORS(app)
 
 # Configurações do S3
 S3_BUCKET = 'api-iot-estacio'
-S3_REGION = 'sa-east'  # Ex: us-east-1
+S3_REGION = 'sa-east-1'  # Ex: us-east-1
 S3_ACCESS_KEY = 'AKIAW3MEDVA6ASTQFPLM'
 S3_SECRET_KEY = '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'
 
@@ -23,34 +25,43 @@ s3_client = boto3.client(
 
 @app.route('/register', methods=['POST'])
 def create_user():
-  # Receber dados do JSON
-  data = request.get_json()
-  nome = data.get('nome')
-  email = data.get('email')
-  telefone = data.get('telefone')
-  
-  # Gerar UUID para o usuário
-  user_id = str(uuid.uuid4())
-  
-  # Verificar se o arquivo foi enviado com a requisição
-  if 'foto' not in request.files:
-    return jsonify({'error': 'Nenhuma foto enviada'}), 400
-  
-  foto = request.files['foto']
-  
-  # Salvar o arquivo com um nome seguro e único usando o UUID
-  filename = f"{user_id}_{secure_filename(foto.filename)}"
-  foto_path = os.path.join('/tmp', filename)
-  foto.save(foto_path)
-
-  # Fazer upload da foto para o S3
   try:
-    s3_client.upload_file(
-      foto_path,
-      S3_BUCKET,
-      f"usuarios/{filename}",
-      ExtraArgs={'ACL': 'public-read'}  # Permite acesso público ao arquivo
-    )
+    # Receber dados do formulário
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    telefone = request.form.get('telefone')
+
+    # Verificar se os campos obrigatórios foram enviados
+    if not all([nome, email, telefone]):
+      return jsonify({'error': 'Nome, email e telefone são obrigatórios'}), 400
+
+    # Verificar se o arquivo foi enviado com a requisição
+    if 'foto' not in request.files:
+      return jsonify({'error': 'Nenhuma foto enviada'}), 400
+
+    foto = request.files['foto']
+
+
+    # Gerar UUID para o usuário
+    user_id = str(uuid.uuid4())
+
+
+    # Salvar o arquivo com um nome seguro e único usando o UUID
+    filename = f"{user_id}_{secure_filename(foto.filename)}"
+    foto_path = os.path.join('./tmp', filename)
+    foto.save(foto_path)
+
+    # Fazer upload da foto para o S3
+    try:
+      s3_client.upload_file(
+          foto_path,
+          S3_BUCKET,
+          f"usuarios/{filename}"
+      )
+      print('Upload para S3 bem-sucedido')
+    except Exception as e:
+      print(f"Erro ao fazer upload para o S3: {str(e)}")
+      return jsonify({'error': f"Erro ao fazer upload para o S3: {str(e)}"}), 500
     
     # Gerar a URL pública da foto
     foto_url = f"https://{S3_BUCKET}.s3.{S3_REGION}.amazonaws.com/usuarios/{filename}"
@@ -63,9 +74,10 @@ def create_user():
       'telefone': telefone,
       'foto_url': foto_url
     }
+
     # Converter dados do usuário para JSON
     json_filename = f"{user_id}_dados.json"
-    json_path = os.path.join('/tmp', json_filename)
+    json_path = os.path.join('./tmp', json_filename)
     with open(json_path, 'w') as json_file:
       json.dump(new_user_data, json_file)
     
@@ -73,8 +85,7 @@ def create_user():
     s3_client.upload_file(
       json_path,
       S3_BUCKET,
-      f"usuarios/{json_filename}",
-      ExtraArgs={'ACL': 'public-read'}  # Permite acesso público ao arquivo
+      f"usuarios/{json_filename}"
     )
     
     # URL pública do arquivo JSON
